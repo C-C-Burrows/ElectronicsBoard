7cec19d4fa98f3b15e5ebe1fe0c8915124b473ae
C-C-Burrows <99225568+C-C-Burrows@users.noreply.github.com>
2022-02-26 10:41:54 +1100

    Create week_1.ino

---
+++ week_1/week_1.ino
@@ -0,0 +1,23 @@
+// C++ code
+//
+/*
+  This program blinks pin 13 of the Arduino (the
+  built-in LED)
+*/
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600); // open the serial port at 9600 bps:
+}
+
+void loop()
+{
+  Serial.println("tick");
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+}
